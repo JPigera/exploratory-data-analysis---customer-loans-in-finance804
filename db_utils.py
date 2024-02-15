4f3c783a4a44ae40eca4fd13eea17ec46f749d38
Your Name <jpigera45@gmail.com>
2024-02-15 15:35:13 +0000

    Creating an RDS Database Connector class, including methods to access the database, get the table names, extract the data from the chosen table, save the data to a csv file and show a sample of the data. Then creating an instance of the class to test each method. There are no errors at this stage.

--- db_utils.py
+++ db_utils.py
@@ -1,27 +1,72 @@
 import yaml
 import pandas as pd
+from sqlalchemy import create_engine, inspect
+import psycopg2
+
 
 class RDSDatabaseConnector:
 
-    def readcreds():
-        with open('credentials.yaml', 'r') as file:
-            creds = yaml.safe_load(file)
-        return creds
-
     def __init__(self, creds: dict):
-        self.creds = self.readcreds()
+        self.creds = creds
 
-    def create_engine(self):
+    def db_engine(self):
+
+        with open(self.creds, 'r') as file:
+            self.creds_dict = yaml.safe_load(file)
+    
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
-        ENDPOINT = self.creds['RDS_HOST']
-        USER = self.creds['RDS_USER']
-        PASSWORD = self.creds['RDS_PASSWORD']
+        ENDPOINT = self.creds_dict['RDS_HOST']
+        USER = self.creds_dict['RDS_USER']
+        PASSWORD = self.creds_dict['RDS_PASSWORD']
         PORT = 5432
-        DATABASE = self.creds['RDS_DATABASE']
-        engine = self.create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+        DATABASE = self.creds_dict['RDS_DATABASE']
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
         return engine
     
-    def extractdata():
-        self.data = self.pandas.read_sql()
-        return self.data
+    def get_table_names(self, engine):
+
+        inspector = inspect(engine)
+
+        return inspector.get_table_names()
+    
+    def extract_data(self, engine):
+
+        loan_payments_table = pd.read_sql('loan_payments', engine)
+
+        return loan_payments_table
+    
+    def save_data(self, loan_payments_table):
+
+        loan_payments_table.to_csv('df.csv')
+
+        return
+    
+    def show_data(self, loan_payments_table):
+        
+        
+
+        data_frame = pd.DataFrame(data=loan_payments_table)
+
+        pd.set_option('display.max_rows', 4)
+
+        pd.set_option('display.max_columns', None)
+
+        print(data_frame)
+        
+        return
+    
+
+test = RDSDatabaseConnector('credentials.yaml')
+
+engine= test.db_engine()
+
+table_names = test.get_table_names(engine)
+
+print(table_names)
+
+loan_payments_table = test.extract_data(engine)
+
+test.save_data(loan_payments_table)
+
+test.show_data(loan_payments_table)
